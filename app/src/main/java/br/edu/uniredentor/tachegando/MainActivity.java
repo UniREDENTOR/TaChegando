70ec11e9047e5d7ac4880fb9176e6d6fd80a2b3f
Raphael Ramos <raphaelframos@gmail.com>
2019-09-20 19:00:05 -0300

    Criando projeto

---
+++ app/src/main/java/br/edu/uniredentor/tachegando/MainActivity.java
@@ -0,0 +1,13 @@
+package br.edu.uniredentor.tachegando;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
