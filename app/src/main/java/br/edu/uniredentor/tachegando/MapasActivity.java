257ca12ea2869f1a40cf5e99140a2b1b50d5241e
Raphael Ramos <raphaelframos@gmail.com>
2019-09-20 23:42:02 -0300

    Método atualiza onibus

--- app/src/main/java/br/edu/uniredentor/tachegando/MapasActivity.java
+++ app/src/main/java/br/edu/uniredentor/tachegando/MapasActivity.java
@@ -3,12 +3,17 @@ package br.edu.uniredentor.tachegando;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapasActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -22,13 +27,23 @@ public class MapasActivity extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+
+    private void atualizaOnibus() {
+
+
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(-21.2030985, -41.8884492);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f));
+
+
     }
 }
