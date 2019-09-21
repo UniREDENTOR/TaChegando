ad7e3c5d84c0fbb02736558c67c696b3ae5e34ef
Raphael <raphaelframos@gmail.com>
2019-09-21 00:17:53 -0300

    Capturando localizacao do usuario

--- app/src/main/java/br/edu/uniredentor/tachegando/MapasActivity.java
+++ app/src/main/java/br/edu/uniredentor/tachegando/MapasActivity.java
@@ -1,11 +1,20 @@
 package br.edu.uniredentor.tachegando;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,10 +24,14 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapasActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int CODIGO_PERMISSAO = 123;
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +43,37 @@ public class MapasActivity extends FragmentActivity implements OnMapReadyCallbac
 
     }
 
-    private void atualizaOnibus() {
+    private void getMinhaLocalizacao() {
+        fusedLocation = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            final Task localizacao = fusedLocation.getLastLocation();
+            localizacao.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        Location localizacaoAtual = (Location) task.getResult();
+                        moveCamera(new LatLng(localizacaoAtual.getLatitude(), localizacaoAtual.getLongitude()));
+                    }
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    private boolean possuiPermissao(){
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED){
+            String[] permissoes = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            ActivityCompat.requestPermissions(MapasActivity.this, permissoes, CODIGO_PERMISSAO);
+            return false;
+        }
+        return true;
+    }
 
-
+    private void moveCamera(LatLng latLng){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f));
     }
 
     @Override
@@ -43,6 +82,10 @@ public class MapasActivity extends FragmentActivity implements OnMapReadyCallbac
         LatLng sydney = new LatLng(-21.2030985, -41.8884492);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f));
+        if(possuiPermissao()){
+            mMap.setMyLocationEnabled(true);
+            getMinhaLocalizacao();
+        }
 
 
     }
