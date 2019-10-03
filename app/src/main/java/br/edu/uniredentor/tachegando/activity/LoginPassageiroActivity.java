190424c07bf3e94c4ff55986f336035c3c8513ed
gustavo <gustavo10ssouza@gmail.com>
2019-10-03 18:08:40 -0300

    add activity login e verificar

---
+++ app/src/main/java/br/edu/uniredentor/tachegando/activity/LoginPassageiroActivity.java
@@ -0,0 +1,62 @@
+package br.edu.uniredentor.tachegando.activity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+import br.edu.uniredentor.tachegando.R;
+
+public class LoginPassageiroActivity extends AppCompatActivity {
+
+    private EditText editTextNumero;
+
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_passageiro);
+
+        getSupportActionBar().setTitle("Login");
+        getSupportActionBar().setElevation(0);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#6A5ACD")));
+
+        auth = FirebaseAuth.getInstance();
+
+
+        editTextNumero = findViewById(R.id.editTextMobile);
+
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextNumero.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextNumero.setError("Entre com número válido");
+                    editTextNumero.requestFocus();
+                    return;
+                }
+
+                //enviando o numero do telefone para o verificarlogin.class
+                Intent intent = new Intent(getApplicationContext(), VerificarLoginPassageiroActivity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
