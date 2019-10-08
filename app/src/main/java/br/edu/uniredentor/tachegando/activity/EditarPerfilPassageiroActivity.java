4aaf19b07d75e7a0aae9a91eeaac48fcb20b8fa7
gustavo <gustavo10ssouza@gmail.com>
2019-10-08 00:46:50 -0300

    implementa funcao de acesso a galeria

--- app/src/main/java/br/edu/uniredentor/tachegando/activity/EditarPerfilPassageiroActivity.java
+++ app/src/main/java/br/edu/uniredentor/tachegando/activity/EditarPerfilPassageiroActivity.java
@@ -1,6 +1,9 @@
 package br.edu.uniredentor.tachegando.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -11,23 +14,29 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import java.io.IOException;
+
 import br.edu.uniredentor.tachegando.R;
 
 public class EditarPerfilPassageiroActivity extends FragmentActivity {
 
-    private ImageView imageViewEditarFotoPerfil, imageViewFotoPerfil, imageViewTelefone, imageViewNome;
+    private ImageView imageViewBtnEditarFotoPerfil, imageViewFotoPerfil, imageViewTelefone, imageViewNome;
     private TextView textViewNomeEditarPerfil, textViewTelefoneEditarPerfil;
+    private Uri fotoSelecionada;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil_passageiro);
 
-        imageViewEditarFotoPerfil = findViewById(R.id.imageView_edit_foto_perfil_passageiro);
+        imageViewFotoPerfil = findViewById(R.id.imageView_foto_passsageiro_editar_perfil);
+        imageViewBtnEditarFotoPerfil = findViewById(R.id.imageView_edit_foto_perfil_passageiro);
 
-        imageViewEditarFotoPerfil.setOnClickListener(new View.OnClickListener() {
+        imageViewBtnEditarFotoPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                selecionarFoto();
             }
         });
 
@@ -35,5 +44,29 @@ public class EditarPerfilPassageiroActivity extends FragmentActivity {
 
         Toolbar toolbarEditarPerfil = findViewById(R.id.toolbar_principal);
         toolbarEditarPerfil.setTitle(R.string.editar);
+    }
+
+    private void selecionarFoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0) {
+            fotoSelecionada = data.getData();
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), fotoSelecionada);
+                imageViewFotoPerfil.setImageDrawable(new BitmapDrawable(bitmap));
+                imageViewBtnEditarFotoPerfil.setAlpha(0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
