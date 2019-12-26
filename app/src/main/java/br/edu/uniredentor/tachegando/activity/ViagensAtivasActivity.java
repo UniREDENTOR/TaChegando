b02b771bc2f78a6f27c51b83c9831a7ddfa40821
gussouzauni <gustavo10ssouza@gmail.com>
2019-12-26 18:40:07 -0300

    remove layout verificar login

---
+++ app/src/main/java/br/edu/uniredentor/tachegando/activity/ViagensAtivasActivity.java
@@ -0,0 +1,18 @@
+package br.edu.uniredentor.tachegando.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.edu.uniredentor.tachegando.R;
+
+public class ViagensAtivasActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viagens_ativas);
+    }
+}
